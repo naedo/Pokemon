5c299ab213addbcf3da54668656cca813f0b2fa3
deanlc <d.chalk@students.rave.ac.uk>
2017-01-08 17:56:51 +0000

    adding learnset

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 var evoButton = $('#evo');
 var effButton = $('#eff');
+var learnButton = $('#learn');
+var searchbox = $('#searchbox');
 
 evoButton.click(function () {
     // capture the user chosen option
-    var searchbox = $('#searchbox');
     var search = $('#searchbox').val();
     
     console.log("You picked " + search);
@@ -12,9 +13,17 @@ evoButton.click(function () {
 
 effButton.click(function () {
     // capture the user chosen option
-    var searchbox = $('#searchbox');
     var search = $('#searchbox').val();
     
     console.log("You picked " + search);
     window.open('http://bulbapedia.bulbagarden.net/wiki/' + search + '_(Pok%C3%A9mon)#Type_effectiveness','_blank');
+});
+
+learnButton.click(function () {
+    // capture the user chosen option
+    var search = $('#searchbox').val();
+    var gen = $('#genSelect').val();
+    
+    console.log("Search: " + search + " | Gen: " + gen);
+    window.open('http://bulbapedia.bulbagarden.net/wiki/' + search + '_(Pok%C3%A9mon)/Generation_' + gen + '_learnset#firstHeading','_blank');
 });
